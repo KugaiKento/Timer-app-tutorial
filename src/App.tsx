433765b017474f06c680abd41e4b366a33631c83
KentoKugai <145778437+KugaiKento@users.noreply.github.com>
2025-10-27 20:47:15 +0900

    一旦コンポーネントも削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,7 @@
-import React from "react";
-import { useStopwatch } from "./hooks/useStopWatch";
+// import React from 'react'
 
-const Stopwatch: React.FC = () => {
-  const { time, isRunning, start, pause, reset } = useStopwatch();
+function App() {
+  return <div>App</div>;
+}
 
-  const milliseconds = `0${Math.floor((time % 1000) / 10)}`.slice(-2);
-  const seconds = `0${Math.floor(time / 1000) % 60}`.slice(-2);
-  const minutes = `0${Math.floor(time / 60000) % 60}`.slice(-2);
-  const hours = `0${Math.floor(time / 3600000)}`.slice(-2);
-
-  return (
-    <div>
-      <h1>Stopwatch</h1>
-      <p>
-        {hours}:{minutes}:{seconds}:{milliseconds}
-      </p>
-      {isRunning ? (
-        <button onClick={pause}>Pause</button>
-      ) : (
-        <button onClick={start}>Start</button>
-      )}
-      <button onClick={reset}>Reset</button>
-    </div>
-  );
-};
-
-export default Stopwatch;
+export default App;
